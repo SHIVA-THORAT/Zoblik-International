577e1fe9bf8ad400d715aa96e367d197f0140f44
Shivaji Thorat <shiva.thorat2002@gmail.com>
2026-01-19 11:17:53 +0530

    Initial commit: Node.js CLI program

---
+++ welcome.js
@@ -0,0 +1,7 @@
+const Name = process.argv[2];
+
+if (process.argv.length <= 2) {
+    console.log("Welcome to the platform, Shivaji Thorat ! Let's begin your JavaScript journey.");
+} else {
+    console.log(`Welcome to the platform,${Name}! Let's begin your JavaScript journey.`);
+}
